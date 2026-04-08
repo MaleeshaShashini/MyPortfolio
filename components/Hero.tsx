506be9e63e87eebc622410469d5e4d221e13525e
MaleeshaShashini <maleeshashashini70@gmail.com>
2026-04-08 19:46:10 +0530

    feat: implement static navbar UI with dynamic navLinks mapping

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Hero = () => {
     return(
-        <div id="hero" className="p-10 text-white border border-white/10 rounded-xl">
+        <div id="home" className="p-10 text-white border border-white/10 rounded-xl">
             <h2 className="text-2xl font-bold"> Hero</h2>
             <p>This is Hero Section.</p>
         </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 
 export const Navbar = () => {
+
+    const navLinks = [
+        {name:'Home', id:'home'},
+        {name:'About',id:'about'},
+        {name:'Education',id:'education'},
+        {name:'Skills',id:'skills'},
+        {name:'Project',id:'project'},
+        {name:'Contact',id:'contact'},
+    ];
+
     return(
-        <div id="navbar" className="p-10 text-white border border-white/10 rounded-xl">
-            <h2 className="text-2xl font-bold"> Navbar</h2>
-            <p>This is Navbar Section.</p>
-        </div>
+        <nav id="navbar" className="p-6 bg-[#00171f] flex justify-between item-center border-b border-white/10">
+            <h1 className="text-xl font-bold text-white">MALEESHA SHASHINI</h1>
+
+            <div className="flex gap-6">
+                {navLinks.map((link) =>(
+                    <button
+                        key={link.id}
+                        className="text-gray-300 hover:text-white transition-colors"
+                    >
+                        {link.name}
+                    </button>
+                ))} 
+            </div>
+        </nav>
     );
 };
